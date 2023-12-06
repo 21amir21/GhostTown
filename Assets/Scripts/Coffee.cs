50b2e9b2303836bd05cef538b185986d1f3dd33f
prisklla <140534986+prisklla@users.noreply.github.com>
2023-12-06 14:33:32 +0200

    adding codes
    adding many code files

---
+++ Assets/Scripts/Coffee.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coffee : MonoBehaviour
+{
+    public int damage = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+            if (other.tag == "Player")
+            {
+                FindObjectOfType<PlayerStats>().TakeDamage(damage);
+            }
+
+        }
+    
+}
