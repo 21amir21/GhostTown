c7aa9ccdcf54be33fdc28a6520be5a6eb957a841
prisklla <140534986+prisklla@users.noreply.github.com>
2023-12-11 22:44:56 +0200

    Police Scene 2
    Completed Features:
    
    Police Chase and Shoot Mechanics:
    
    Successfully implemented a system for two police characters to chase and shoot at the player when within a certain distance.
    Hot Coffee Interaction:
    
    Developed a hot coffee element in the game; however, the current implementation only registers interaction when the player is moving and touches the coffee, not when standing.
    Player Shooting Mechanism:
    
    Implemented the ability for the player to shoot projectiles.
    To Be Implemented:
    
    Hot Coffee Interaction (Standing):
    
    Address the issue where the hot coffee element only works when the player is moving and touching it, not when standing.
    Projectile Directionality:
    
    Enhance the projectile directionality to align with the player's movement, ensuring proper orientation (left or right).
    Enemy Damage System:
    
    Implement a system for the player's projectiles to cause damage to enemies, fostering a dynamic interaction between player and enemy entities.
    Idle Police Eating Donuts:
    
    Introduce an idle action for a police character, allowing them to eat donuts and adding a humorous and interactive element to the game.
    Visible and Invisible Boxes:
    
    Further define the purpose and interaction of visible and invisible box elements in the game.

--- Assets/Scripts/Coffee.cs
+++ Assets/Scripts/Coffee.cs
@@ -8,17 +8,17 @@ public class Coffee : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void OnCollisionStay2D(Collision2D other)
     {
-        
+
         if (other.collider.tag == "Player")
         {
             FindObjectOfType<PlayerStats>().TakeDamage(damage);
