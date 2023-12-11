54239424fa7138025e642427c0d90305091d4c17
CordialBeatle0 <pitosameh@hotmail.com>
2023-12-11 21:10:05 +0200

    fixed files
    fixed files

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,46 +4,26 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject CurrentCheckpoint; //we can update the current checkpoint from within Unity
-    public Transform player;
+	public GameObject CurrentCheckpoint; //we can update the current checkpoint from within Unity
+	public Transform player;
 
-    // public Transform Enemy;
-    public Transform Enemy;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void RespawnPlayer()
-    {
-        FindObjectOfType<PlayerMovement>().transform.position = CurrentCheckpoint.transform.position;
-        //Search for the asset/object called Controller (ur player's script code name whatever it is).
-        //once u've found it, change its player game object's position to be at the last checkpoint the
-        //player passed through before s/he died ..
-        
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
 
-    }
-
-
-    public void RespawnEnemy()
-    {
-        Instantiate(Enemy, rock.transform.position, rock.transform.rotation);
-
-    }
-    public void PoliceManKilled()
-    {
-        policeKilled++;
-        if (policeKilled >= 2)
-        {
-            invBox.SetActive(true);
-        }
-    }
-
+	public void RespawnPlayer()
+	{
+		FindObjectOfType<PlayerMovement>().transform.position = CurrentCheckpoint.transform.position;
+		//Search for the asset/object called Controller (ur player's script code name whatever it is).
+		//once u've found it, change its player game object's position to be at the last checkpoint the
+		//player passed through before s/he died ..
+	}
 }
