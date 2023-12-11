00154777a885a7d8ad24bc943dbb1f67c0d7daa7
SheroukHesham <sheroukhesham2000@hotmail.com>
2023-12-11 16:02:08 +0200

    Revert "adding Stashs"
    This reverts commit 7b5945257ae74adbdde844c9d1fb4a8c569b2448.

--- Assets/Scripts/FishingNet.cs
+++ Assets/Scripts/FishingNet.cs
@@ -24,13 +24,8 @@ public class FishingNet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-<<<<<<< Updated upstream
         if (collision.tag == "SwimPlayer" )
             hitCount++;
-=======
-        hitCount++;
-        if (collision.tag == "SwimPlayer")
->>>>>>> Stashed changes
         {
             //if player aquired crab
             if (FindObjectOfType<SwimmingMovement>().aquiredCrab)
